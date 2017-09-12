f485c302d69171884bdd58a78306d458ce58e86f
Brian Fong <brianwkfong@gmail.com>
2017-09-11 22:45:46 -0700

    first commit

---
+++ Assets/Scripts/TriggerLevelLoad.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerLevelLoad : MonoBehaviour {
+
+	public string nameOfLevelToLoad  = "";
+    public int advanceLevel = 10;
+    public int count;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.tag == "Pickup")
+		{
+            count++;
+            if(count == advanceLevel)
+            {
+                Application.LoadLevel(nameOfLevelToLoad);
+            }
+		}
+	}
+}
